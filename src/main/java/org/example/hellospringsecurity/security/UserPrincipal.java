e9bbeb46e97c74b98f76cc52180c4095ddabc026
lequoc <lequoc150305@quoc150305gmail.com>
2026-06-08 18:18:01 +0700

    Custom lại UserDetails với UserPrincipal

---
+++ src/main/java/org/example/hellospringsecurity/security/UserPrincipal.java
@@ -0,0 +1,61 @@
+package org.example.hellospringsecurity.security;
+import org.example.hellospringsecurity.model.User;
+import org.springframework.security.core.GrantedAuthority;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserPrincipal implements UserDetails {
+
+    private User user;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserPrincipal(
+            User user,
+            Collection<? extends GrantedAuthority> authorities) {
+
+        this.user = user;
+        this.authorities = authorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
